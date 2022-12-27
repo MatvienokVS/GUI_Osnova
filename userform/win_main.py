3d9a11191721192cce85b67353fa8c67d5a658c1
MatvienokVS <skavaava@gmail.com>
2022-12-27 23:14:24 +0300

    Создание меню первого окна

---
+++ userform/win_main.py
@@ -0,0 +1,27 @@
+# Создание главной формы проекта GUI_Osnova
+
+import tkinter as tk
+from tkinter import *
+import os
+import sys
+import imp
+from 'menu\' import menu_main
+
+
+win_main = tk.Tk()
+
+heidth = 1500
+width = 800
+
+
+win_main.geometry(f'{heidth}x{width}+200+100')			# Геометрия окна
+win_main.config(bg='#00abdd')							# Фон окна
+win_main.title('Бюджет движения денежных средств')		# Название окна
+win_main.resizable(False, False)						# Запрет на изменение размера
+icon = PhotoImage(file='userform/photo.png')			# Добавление иконки
+win_main.iconphoto(False, icon)
+
+menu_main()
+
+
+win_main.mainloop()
